815a892149774ee2ce5fd89da608206a1ab3195c
Mamadou Cisse <mciissee@gmail.com>
2020-06-25 15:21:23 +0200

    feat(libs): add shared libs

--- apps/platon/src/app/app.module.ts
+++ apps/platon/src/app/app.module.ts
@@ -1,12 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
+// ANGULAR
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// LIBS
+import { SharedVendorsModule } from '@platon/shared/vendors';
 
 import { AppComponent } from './app.component';
-
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        SharedVendorsModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ libs/shared/vendors/src/lib/shared-vendors.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+// Angular Material
+import 'hammerjs';
+
+/**
+ * 3rd party librairies integrations in the project.
+ */
+@NgModule({})
+export class SharedVendorsModule {}
