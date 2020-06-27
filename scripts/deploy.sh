4b018082f85fe70a8470ab9e8991da304eb149d8
Mamadou Cisse <mciissee@gmail.com>
2020-06-28 01:19:04 +0200

    refactor: move ghpage deploy optionss to angular.json

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
 # https://www.npmjs.com/package/angular-cli-ghpages#configuration
-./scripts/build.sh ghpage && ng deploy --project=ghpage --no-build --base-href=https://premierlangage.github.io/platon-front/
+./scripts/build.sh ghpage && ng deploy --project=ghpage --dry-run
