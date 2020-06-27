ddb9c2cccbe3ac9eea204077736221e51caa7acc
Mamadou Cisse <mciissee@gmail.com>
2020-06-27 22:28:56 +0200

    fix(webcomponents): remove unused declaration in webcomponents registry

--- libs/webcomponents/src/lib/web-components-registry.ts
+++ libs/webcomponents/src/lib/web-components-registry.ts
@@ -2,5 +2,4 @@ import { LazyComponentDef } from '@juristr/ngx-lazy-el';
 
 export const WEB_COMPONENTS_REGISTRY: LazyComponentDef[] = [
     { selector: 'wc-text-input', loadChildren: () => import('./text-input/text-input.module').then(m => m.TextInputModule) },
-    { selector: 'wc-ttt', loadChildren: () => import('./ttt/ttt.module').then( m => m.TttModule) },
 ];
