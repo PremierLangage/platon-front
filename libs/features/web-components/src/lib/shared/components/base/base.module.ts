40f480e7239883fa6ffa74770b4507f13f6b49f1
Mamadou Cisse <mciissee@gmail.com>
2020-10-24 15:13:30 +0200

    refactor: remove unused imports

--- libs/features/web-components/src/lib/shared/components/base/base.module.ts
+++ libs/features/web-components/src/lib/shared/components/base/base.module.ts
@@ -1,13 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ObserversModule } from '@angular/cdk/observers';
 import { NgeMonacoModule } from 'nge-monaco';
 import { BaseComponent } from './base.component';
 
 @NgModule({
     imports: [
         CommonModule,
-        ObserversModule,
         NgeMonacoModule,
     ],
     exports: [
