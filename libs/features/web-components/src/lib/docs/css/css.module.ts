89ecae1cf22ec6ac9efb1da6e8cde9ce98c26958
Mamadou Cisse <mciissee@gmail.com>
2020-10-25 18:45:24 +0100

    docs: update

---
+++ libs/features/web-components/src/lib/docs/css/css.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CssComponent } from './css.component';
+import { NgeMarkdownModule } from 'nge-markdown';
+import { SharedUiDialogModule } from '@platon/shared/ui';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    NgeMarkdownModule,
+    SharedUiDialogModule,
+  ],
+  declarations: [CssComponent]
+})
+export class CssModule {
+    component = CssComponent;
+}
--- libs/features/web-components/src/lib/docs/docs.ts
+++ libs/features/web-components/src/lib/docs/docs.ts
@@ -18,6 +18,9 @@ export const WebComponentsDocRoute =  {
             },
         },
         pages: [
+            { title: 'Présentation', href: 'presentation', renderer: 'assets/docs/components/docs/presentation.md' },
+            { title: 'Utilisation', href: 'usage', renderer: 'assets/docs/components/docs/usage.md'  },
+            { title: 'API CSS', href: 'css-api', renderer: () => import('./css/css.module').then(m => m.CssModule) },
             (injector: any) => {
                 const api = injector.get(WebComponentsService);
                 return {
