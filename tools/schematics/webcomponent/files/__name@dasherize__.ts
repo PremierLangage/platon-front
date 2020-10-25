b7f2e29d003eec057a7dc86ecae86faea10ede9d
Mamadou Cisse <mciissee@gmail.com>
2020-10-25 13:45:54 +0100

    fix: webcomponent schematic generate wrong icon and desc urls

--- tools/schematics/webcomponent/files/__name@dasherize__.ts
+++ tools/schematics/webcomponent/files/__name@dasherize__.ts
@@ -7,10 +7,10 @@ export interface <%= classify(name) %> extends IWebComponent {
 export const <%= classify(name) %>ComponentDefinition = defineWebComponent({
     type: WebComponentTypes.<%= type %>,
     name: '<%= classify(name) %>',
-    icon: 'assets/images/components/<%= type %>s/<%= classify(name) %>/<%= classify(name) %>.svg',
+    icon: 'assets/images/components/<%= type %>s/<%= name %>/<%= name %>.svg',
     selector: 'wc-<%= name %>',
     description: 'REMPLACEZ CE TEXTE PAR UNE DESCRIPTION DE VOTRE COMPOSANT',
-    fullDescriptionUrl: 'assets/docs/components/<%= type %>s/<%= classify(name) %>/<%= classify(name) %>.md',
+    fullDescriptionUrl: 'assets/docs/components/<%= type %>s/<%= name %>/<%= name %>.md',
     // https://json-schema.org/understanding-json-schema/
     schema: {
         $schema: 'http://json-schema.org/draft-07/schema',
