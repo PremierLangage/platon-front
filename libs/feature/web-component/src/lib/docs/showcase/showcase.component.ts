9fb37cfb969a9933c4cc7c645c29cec54d7df1d6
Mamadou Cisse <mciissee@gmail.com>
2020-11-10 22:16:11 +0100

    refactor: rename features-web-components lib to feature-web-component

---
+++ libs/feature/web-component/src/lib/docs/showcase/showcase.component.ts
@@ -0,0 +1,62 @@
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AssetLoaderService } from '@platon/shared/utils';
+import { JsonEditorComponent, JsonEditorOptions } from 'ang-jsoneditor';
+import { WebComponentDefinition } from '../../web-component';
+
+@Component({
+    selector: 'wc-showcase',
+    templateUrl: './showcase.component.html',
+    styleUrls: ['./showcase.component.scss'],
+})
+export class ShowcaseComponent implements OnInit {
+    @Input() definition!: WebComponentDefinition;
+
+    @ViewChild(JsonEditorComponent)
+    readonly editor?: JsonEditorComponent;
+    readonly options = new JsonEditorOptions();
+
+    showEditor = true;
+    component?: any;
+
+    constructor(
+        private readonly el: ElementRef<HTMLElement>,
+        private readonly assets: AssetLoaderService,
+    ) {}
+
+    async ngOnInit() {
+        this.assets.loadAllAsync([
+            ['style', 'assets/vendors/jsoneditor/jsoneditor.min.css']
+        ]).toPromise().catch();
+
+        const host = this.el.nativeElement.firstElementChild;
+        this.component = document.createElement(
+            this.definition.selector
+        ) as any;
+        if (this.definition.showcase) {
+            this.component.state = this.definition.showcase;
+        }
+        host?.appendChild(this.component);
+
+        this.options.modes = ['tree'/* , 'view', 'form', 'code', 'text' */];
+        this.options.language = 'fr-FR';
+        this.options.schema = this.definition.schema;
+        this.options.mainMenuBar = false;
+        this.options.sortObjectKeys = true;
+    }
+
+    editorToComponent() {
+        try {
+            const state = this.editor?.get();
+            if (state && this.component) {
+                this.component.state = state;
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    componentToEditor() {
+        this.editor?.set(this.component.state);
+    }
+
+}
--- libs/features/web-components/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/features-web-components.module';
-export * from './lib/web-components';
-export * from './lib/web-components-registry';
-export * from './lib/web-components.service';
-export * from './lib/docs/docs';
