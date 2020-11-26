99c1ec39974369fb513e3c5baaddd44083be7e5e
Mamadou Cisse <mciissee@gmail.com>
2020-11-26 19:16:26 +0100

    feat: add dynamic injector service

--- libs/shared/utils/src/index.ts
+++ libs/shared/utils/src/index.ts
@@ -2,4 +2,5 @@ export * from './lib/pipes/icongr.pipe';
 export * from './lib/pipes/safe.pipe';
 export * from './lib/services/asset-loader.service';
 export * from './lib/services/clipboard.service';
+export * from './lib/services/dynamic-injector.service';
 export * from './lib/utils/objects';
---
+++ libs/shared/utils/src/lib/services/dynamic-injector.service.ts
@@ -0,0 +1,29 @@
+import {
+    AbstractType,
+    Injectable,
+    InjectionToken,
+    Injector,
+    Type,
+} from '@angular/core';
+
+export interface IDynamicService {
+    injectable(): boolean;
+}
+
+export interface IDynamicInjectorConfig {
+    contextToken: Type<any> | InjectionToken<any> | AbstractType<any>;
+}
+
+@Injectable({ providedIn: 'root' })
+export class DynamicInjectorService {
+
+    constructor(private readonly injector: Injector) {}
+
+    get<T extends IDynamicService>(
+        token: Type<any> | InjectionToken<any> | AbstractType<any>
+    ) {
+        return (this.injector.get(token, []) as T[]).find((e) =>
+            e.injectable()
+        );
+    }
+}
