08a8487c97aa9a933a514cffd74387e6bf504b3e
Mamadou Cisse <mciissee@gmail.com>
2020-11-28 13:30:04 +0100

    refactor: update config service to test if server is running

--- libs/shared/utils/src/lib/services/config.service.ts
+++ libs/shared/utils/src/lib/services/config.service.ts
@@ -5,13 +5,7 @@ import { Injectable } from '@angular/core';
  */
 @Injectable({providedIn: 'root'})
 export class ConfigService {
-    private demo = true;
-
-    get isDemo(): boolean {
-        return this.demo;
-    }
-
-    set isDemo(value: boolean) {
-        this.demo = value;
+    get isServerRunning(): boolean {
+        return document.body.getAttribute('platon-api-version') != null;
     }
 }
