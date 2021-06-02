3ee05a32ac8aa259efacba3f5ef10b09470ac25d
Mamadou Cisse <mciissee@gmail.com>
2021-06-02 20:08:44 +0200

    refactor: disable dashboard intro

--- apps/platon/src/app/pages/dashboard/dashboard.component.ts
+++ apps/platon/src/app/pages/dashboard/dashboard.component.ts
@@ -29,6 +29,7 @@ export class DashboardComponent implements OnInit {
 
     async ngOnInit() {
         this.user = await this.authService.ready();
+        /*
         setTimeout(async () => {
             const intro = await this.introService.create();
             intro.setOptions({
@@ -68,6 +69,7 @@ export class DashboardComponent implements OnInit {
             intro.addHints()
             intro.start();
         });
+        */
     }
 
 }
