8702e1e9f3d92e3792dbba58b862a95ab61cbd8e
Mamadou Cisse <mciissee@gmail.com>
2021-06-14 16:29:50 +0200

    refactor: create reusable tabbed layout page

--- apps/platon/src/app/shared/layout/layout.module.ts
+++ apps/platon/src/app/shared/layout/layout.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { LayoutModule as CdkLayoutModule } from '@angular/cdk/layout';
+import { LayoutModule } from '@angular/cdk/layout';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatBadgeModule } from '@angular/material/badge';
@@ -11,15 +11,21 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { NzEmptyModule } from 'ng-zorro-antd/empty';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { NzResultModule } from 'ng-zorro-antd/result';
+import { NzSkeletonModule } from 'ng-zorro-antd/skeleton';
 
-import { CoreAuthModule } from '@platon/core/auth';
+import { CoreAuthModule } from '@platon/core/auth';
+import { AppSharedErrorModule } from '../error/error.module';
 
 import { NavComponent } from './nav/nav.component';
 import { LogoComponent } from './logo/logo.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './header/header.component';
 import { DrawerComponent } from './drawer/drawer.component';
 import { ContentComponent } from './content/content.component';
 import { ContainerComponent } from './container/container.component';
+import { ContainerTabbedComponent } from './container-tabbed/container-tabbed.component';
 
 
 const DECLARATIONS = [
@@ -29,6 +35,7 @@ const DECLARATIONS = [
     DrawerComponent,
     ContentComponent,
     ContainerComponent,
+    ContainerTabbedComponent,
 ];
 
 @NgModule({
@@ -36,7 +43,7 @@ const DECLARATIONS = [
         CommonModule,
         RouterModule,
 
-        CdkLayoutModule,
+        LayoutModule,
         MatIconModule,
         MatMenuModule,
         MatBadgeModule,
@@ -45,8 +52,13 @@ const DECLARATIONS = [
         MatToolbarModule,
 
         NzEmptyModule,
+        NzIconModule,
+        NzTabsModule,
+        NzResultModule,
+        NzSkeletonModule,
 
-        CoreAuthModule,
+        CoreAuthModule,
+        AppSharedErrorModule,
     ],
     exports: [
         ...DECLARATIONS,
