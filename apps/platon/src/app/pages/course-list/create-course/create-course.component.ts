1acb6b1cba58adc533efab344d09cd2c5c81925b
JUILLARD-Thomas <51150461+JUILLARD-Thomas@users.noreply.github.com>
2021-07-15 12:02:22 +0200

    feat: :fire: allow to create and display courses

--- apps/platon/src/app/pages/course-list/create-course/create-course.component.ts
+++ apps/platon/src/app/pages/course-list/create-course/create-course.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { CourseService } from '../../course/course.service'
+import { Course } from '../../course/course.service'
+import { Courses } from '../../course/course.service'
+
+
 @Component({
     selector: 'app-create-course',
     templateUrl: './create-course.component.html',
@@ -10,12 +15,34 @@ export class CreateCourseComponent {
     name!: string;
     desc!: string;
     sandbox_name!: string;
-    constructor() {}
+    courses: Course[] = []
 
-    ngOnInit(): void {};
+    constructor(public courseService: CourseService) {}
+
+    ngOnInit(): void {
+        this.courseService.getCourses()
+            .then(response => {
+                if(response){
+                    this.courses = response.courses
+                }
+            })
+            .then(response => {console.log("Get DATA")})
+            .catch(error => console.error(error));
+
+    };
 
     onSubmit() {
         this.submitted = true;
+        if(this.name  && this.desc  && this.sandbox_name){
+            this.courseService.createCourse(this.name.trim(), this.desc.trim(), this.sandbox_name.trim())
+                .then(response => {
+                    if(response){
+                        console.log(response)
+                    }
+                })
+                .then(response => {console.log("log log")})
+                .catch(error => console.error(error));
+        }
     }
 
 
