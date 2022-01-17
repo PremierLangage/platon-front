3b82a3d678298519eb07a89a7abb258405dee247
Mamadou Cisse <mciissee@gmail.com>
2022-01-17 20:36:21 +0100

    feat(premielangage): add editor

--- apps/premierlangage/src/app/pages/editor/editor.component.ts
+++ apps/premierlangage/src/app/pages/editor/editor.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FileService, IdeService } from '@mcisse/nge-ide/core';
+import { PlFileSystem } from './pl-file-system';
+
 
 @Component({
     selector: 'app-editor',
     templateUrl: './editor.component.html',
     styleUrls: ['./editor.component.scss'],
 })
-export class EditorComponent {}
+export class EditorComponent implements OnInit {
+
+    constructor(
+        private readonly ide: IdeService,
+        private readonly http: HttpClient,
+        private readonly fileService: FileService,
+    ) { }
+
+    ngOnInit() {
+        this.ide.onAfterStart(() => {
+            this.fileService.registerProvider(new PlFileSystem(this.http));
+            this.fileService.registerFolders({
+                name: 'home',
+                uri: monaco.Uri.parse('file:///Yggdrasil')
+            }, {
+                name: 'lib',
+                uri: monaco.Uri.parse('file:///lib')
+            });
+        });
+    }
+
+}
