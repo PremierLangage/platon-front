956242b41762b634f8f98afa163850fc3c234af5
Mamadou Cisse <mciissee@gmail.com>
2022-01-21 19:11:33 +0100

    refactor: rename app-container-tabbed to app-tabs

--- apps/platon/src/app/shared/layout/tabs/tabs.component.ts
+++ apps/platon/src/app/shared/layout/tabs/tabs.component.ts
@@ -2,12 +2,12 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { LayoutState, LayoutTab } from '../layout';
 
 @Component({
-    selector: 'app-container-tabbed',
-    templateUrl: './container-tabbed.component.html',
-    styleUrls: ['./container-tabbed.component.scss'],
+    selector: 'app-tabs',
+    templateUrl: './tabs.component.html',
+    styleUrls: ['./tabs.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ContainerTabbedComponent {
+export class TabsComponent {
     @Input()
     tabs: LayoutTab[] = [];
 
