df3a1489b8930029108c614db864a78738715c18
BrotherKey <bkey@bkey-compagnie.local>
2022-08-24 11:40:56 +0200

    fix: section

--- apps/platon/src/app/pages/asset/section/section.component.ts
+++ apps/platon/src/app/pages/asset/section/section.component.ts
@@ -1,9 +1,17 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from "@angular/core";
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { Subscription } from "rxjs";
+import { AuthService } from "@platon/core/auth";
+import { Asset, CoursDetail, CoursService } from "@platon/feature/workspace";
+import { lastValueFrom, Subscription } from "rxjs";
 import { AssetEvents } from "../asset-events.models";
 import { AssetResolver } from "../asset.resolver";
 
+
+import EditorJS, { OutputData } from "@editorjs/editorjs";
+
+declare var Header: any;
+declare var List: any;
+declare var SimpleImage: any;
 
 @Component({
     selector: 'app-asset-section',
@@ -15,11 +23,16 @@ export class SectionComponent extends AssetEvents implements OnInit, OnDestroy {
     private readonly subscriptions: Subscription[] = [];
 
     private id!: number;
+    private cours!: CoursDetail;
+
     section?: Section;
+    editor?:  EditorJS;
 
     constructor(
+        private readonly authService: AuthService,
+        private readonly coursService: CoursService,
         private readonly activatedRoute: ActivatedRoute,
-        private readonly assetResolver: AssetResolver,
+        private readonly changeDetectorRef: ChangeDetectorRef,
     ) {
         super();
         this.subscriptions.push(
@@ -30,17 +43,45 @@ export class SectionComponent extends AssetEvents implements OnInit, OnDestroy {
     }
 
     ngOnInit(): void {
+        this.editor = new EditorJS({
+            holder: 'editor-js',
+            tools: {
+                header: {
+                    class: Header,
+                },
+                list: {
+                    class: List
+                },
+                image: {
+                    class: SimpleImage
+                }
+            },
+            readOnly: true,
+        });
+        this.activatedRoute.parent?.data.subscribe(async data => {
 
+            const [ user, cours ] = await Promise.all([
+                this.authService.ready(),
+                lastValueFrom(this.coursService.getByName(data.asset.name))
+            ]);
+
+            this.cours = cours;
+            this.section = cours.content.sections[this.id];
+            this.editor?.isReady.then(() => {
+                this.editor?.blocks.render(cours.content.sections[this.id].content)
+            });
+            this.changeDetectorRef.markForCheck();
+        }).unsubscribe();
     }
 
     ngOnDestroy(): void {
         this.subscriptions.forEach(s => s.unsubscribe());
+        this.editor?.destroy();
     }
-
 }
 
 export interface Section {
     position: number;
     name: string;
-    content: Record<string, any>;
+    content: OutputData;
 }
